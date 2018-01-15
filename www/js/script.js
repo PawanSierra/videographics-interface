9a44fa2bbd1a74f5fcfab5d6be4bc4bd7140fb82
Mangesh Shelke <mshelke19@gmail.com>
2018-01-15 14:31:37 +0530

    Initial Commit

---
+++ www/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // $(".si-col").height($(window).height() - 100 + 'px');
+  // $(".si-page.login .si-main").height( $(window).height() - 27 + 'px');
+
+  if($('.swiper-container')) {
+    var swiper = new Swiper('.swiper-container',{
+      slidesPerView:10,
+      nextButton:'.swiper-button-next',
+      prevButton:'.swiper-button-prev'
+    });
+  }
+
+});
